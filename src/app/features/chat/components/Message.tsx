b2410cfc1797958158cc5c188afa2e2198a0e5d1
Rohit kumar meena <rohitkumarmeena@Rohits-MacBook-Air.local>
2026-07-09 22:56:54 +0530

    fix

--- src/app/features/chat/components/Message.tsx
+++ src/app/features/chat/components/Message.tsx
@@ -1,8 +1,6 @@
 import { Message as MessageType } from "@app/app/types/chat";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export default function Message({ message }: { message: MessageType }) {
   const isUser = message.role === "user";
@@ -13,30 +11,12 @@ export default function Message({ message }: { message: MessageType }) {
 
   return (
     <div className={`flex w-full ${isUser ? "justify-end" : "justify-start"}`}>
-      <div className={`max-w-full flex flex-col justify-end `}>
+      <div className={`w-full max-w-full flex flex-col justify-end `}>
         <div
           className={`rounded-3xl px-5 py-3 whitespace-pre-wrap
           ${isUser ? "bg-black text-white" : "bg-zinc-100 text-black"}`}
         >
-          <ReactMarkdown
-            remarkPlugins={[remarkGfm]}
-            components={{
-              code(props) {
-                const { children, className } = props;
-                const match = /language-(\w+)/.exec(className || "");
-
-                if (match) {
-                  return (
-                    <SyntaxHighlighter style={oneDark} language={match[1]}>
-                      {String(children).replace(/\n$/, "")}
-                    </SyntaxHighlighter>
-                  );
-                }
-
-                return <code>{children}</code>;
-              },
-            }}
-          >
+          <ReactMarkdown remarkPlugins={[remarkGfm]}>
             {message.content}
           </ReactMarkdown>
         </div>
